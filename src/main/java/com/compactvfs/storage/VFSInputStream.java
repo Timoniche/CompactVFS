8abe6c93f7b9b332e94e8d7a4b2f12b86584b558
timoniche <dmitriidulaev@gmail.com>
2022-01-26 18:54:58 +0300

    readNBytes

--- src/main/java/com/compactvfs/storage/VFSInputStream.java
+++ src/main/java/com/compactvfs/storage/VFSInputStream.java
@@ -9,12 +9,56 @@ public class VFSInputStream implements AutoCloseable {
     private final List<Long> contentChunkPositions;
     private final RandomAccessFile storage;
 
+    private int posInChunkContent = 0;
+    private int chunkIndex = 0;
+
+
     public VFSInputStream(List<Long> contentChunkPositions, String storagePath) throws IOException {
         this.contentChunkPositions = contentChunkPositions;
         if (contentChunkPositions == null || contentChunkPositions.isEmpty()) {
             throw new IOException("No vfsFile content, storage: " + storagePath);
         }
         storage = new RandomAccessFile(storagePath, "r");
+    }
+
+    /**
+     * readNBytes stores last call position
+     * moves position to zero if file is fully read
+     *
+     * @param b size >= n
+     * @return count of read bytes
+     */
+    public int readNBytes(byte[] b, int n) throws IOException {
+        int nChunks = contentChunkPositions.size();
+        int bytesLeft = n;
+        while (bytesLeft > 0) {
+            if (chunkIndex >= nChunks) {
+                seekBegin();
+                return n - bytesLeft;
+            }
+            long chunkInitPos = contentChunkPositions.get(chunkIndex);
+            storage.seek(chunkInitPos);
+            int chunkContentBytesCount = storage.readInt();
+            int chunkContentBytesLeft = chunkContentBytesCount - posInChunkContent;
+
+            storage.seek(chunkInitPos + Integer.BYTES + posInChunkContent);
+            if (bytesLeft < chunkContentBytesLeft) {
+                storage.read(b, n - bytesLeft, bytesLeft);
+                posInChunkContent += bytesLeft;
+                return n;
+            } else {
+                storage.read(b, n - bytesLeft, chunkContentBytesLeft);
+                bytesLeft -= chunkContentBytesLeft;
+                chunkIndex++;
+                posInChunkContent = 0;
+            }
+        }
+        throw new AssertionError("Read must be completed");
+    }
+
+    private void seekBegin() {
+        chunkIndex = 0;
+        posInChunkContent = 0;
     }
 
     public byte[] readAllBytes() throws IOException {
